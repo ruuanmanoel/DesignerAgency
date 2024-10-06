22e2c5500253769d258f96558ceaef70336330a2
Ruan Manoel <ruuanmanoel@gmail.com>
2024-10-06 16:53:56 -0300

    :rocket: implementado design e biblioteca JS

--- main.js
+++ main.js
@@ -13,3 +13,38 @@ navLinks.addEventListener("click", (e)=>{
     navLinks.classList.remove("open")
     menuBtnIcon.setAttribute("class", "ri-menu-line ")
 })
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header_image img",{
+    ...scrollRevealOption,
+    origin: "right",
+})
+ScrollReveal().reveal(".header_content h2",{
+    ...scrollRevealOption,
+    delay:500,
+})
+ScrollReveal().reveal(".header_content h1",{
+    ...scrollRevealOption,
+    delay:1000 ,
+})
+ScrollReveal().reveal(".header_content p",{
+    ...scrollRevealOption,
+    delay:1500,
+})
+ScrollReveal().reveal(".header_content .header_btn",{
+    ...scrollRevealOption,
+    delay:2000,
+})
+ScrollReveal().reveal(".header_content .socials",{
+    ...scrollRevealOption,
+    delay:2500,
+})
+ScrollReveal().reveal(".header_bar",{
+    ...scrollRevealOption,
+    delay:3000,
+})
